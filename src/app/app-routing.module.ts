045b23a58c841d15b6a997d284c68619456eafe9
student <student@>
2024-06-15 16:55:43 +0800

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PTypeCardComponent } from './p-type-card/p-type-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'packages',
+    component: PTypeCardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
